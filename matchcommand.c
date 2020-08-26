525e087f2be42583e677df896bdc9bf2bc8c567a
DavidArias <61300552+gedafu@users.noreply.github.com>
2020-08-25 19:43:59 -0500

    Update matchcommand.c

--- matchcommand.c
+++ matchcommand.c
@@ -16,6 +16,8 @@ char *matchcommand(char *command)
 		if (stat(command, &fileStat) == 0)
 			return (command);
 	folder = _getenv("PATH");
+	if (folders == NULL)
+		return (NULL);
 	folders = splitpath(folder);
 	if (folders != NULL)
 	{
